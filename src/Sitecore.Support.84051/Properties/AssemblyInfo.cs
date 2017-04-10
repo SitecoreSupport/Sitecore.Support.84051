85b0e79f6a52edb3c3f0a1ae5212ce87b55f6f42
DK\ALTU <altu@sitecore.net>
2017-04-10 19:44:49 +0300

    Sitecore.Support.84051 iteration 1

---
+++ src/Sitecore.Support.84051/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.84051")]
+[assembly: AssemblyProduct("Sitecore.Support.84051")]
+[assembly: ComVisible(false)]
